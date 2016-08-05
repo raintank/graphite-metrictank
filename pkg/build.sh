9439112fd5ec398fe761ae2eb4cab57c64f4aee2
woodsaj <anthony@monkey.id.au>
2016-08-05 15:15:57 +0800

    revert to using normal python instead of pypy

--- pkg/build.sh
+++ pkg/build.sh
@@ -3,53 +3,34 @@
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
-
+SOURCEDIR=${DIR}/..
 : ${BUILD_DIR:="${DIR}/build"}
 : ${API_BRANCH:="master"}
 : ${CIRCLE_BRANCH:="master"}
 : ${REPO_PREFIX:="git+https://github.com/raintank"}
 
 # remove any existing BUILD_DIR
-rm -rf ${BUILD_DIR} $DIR/tmp
-mkdir -p $DIR/tmp
+rm -rf ${BUILD_DIR}
 
-wget -O $DIR/tmp/pypy2-v5.3.1-linux64.tar.bz2 https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.3.1-linux64.tar.bz2
-tar -C $DIR/tmp -jxf $DIR/tmp/pypy2-v5.3.1-linux64.tar.bz2
-cd $DIR/tmp/pypy2-v5.3.1-linux64/bin/
-ln -s pypy python
-cd $DIR
 mkdir -p ${BUILD_DIR}/usr/share/python
 
-virtualenv --always-copy -p $DIR/tmp/pypy2-v5.3.1-linux64/bin/python ${BUILD_DIR}/usr/share/python/graphite
-rsync -avhu $DIR/tmp/pypy2-v5.3.1-linux64/ $BUILD_DIR/usr/share/python/graphite/
-cd ${BUILD_DIR}/usr/share/python/graphite/bin
-rm libpypy-c.so
-cp $DIR/tmp/pypy2-v5.3.1-linux64/bin/libpypy-c.so .
-cd $DIR
-
-${BUILD_DIR}/usr/share/python/graphite/bin/pip install -U setuptools pip distribute virtualenv-tools
+virtualenv ${BUILD_DIR}/usr/share/python/graphite
+${BUILD_DIR}/usr/share/python/graphite/bin/pip install -U pip distribute
 ${BUILD_DIR}/usr/share/python/graphite/bin/pip uninstall -y distribute
 
 ${BUILD_DIR}/usr/share/python/graphite/bin/pip install ${REPO_PREFIX}/graphite-api.git@${API_BRANCH}
 ${BUILD_DIR}/usr/share/python/graphite/bin/pip install gunicorn==18.0
-${BUILD_DIR}/usr/share/python/graphite/bin/pip install ${REPO_PREFIX}/graphite-metrictank.git@${CIRCLE_BRANCH}
+${BUILD_DIR}/usr/share/python/graphite/bin/pip install ${SOURCEDIR}
 ${BUILD_DIR}/usr/share/python/graphite/bin/pip install eventlet
 ${BUILD_DIR}/usr/share/python/graphite/bin/pip install git+https://github.com/woodsaj/pystatsd.git
 ${BUILD_DIR}/usr/share/python/graphite/bin/pip install Flask-Cache
 ${BUILD_DIR}/usr/share/python/graphite/bin/pip install python-memcached
+${BUILD_DIR}/usr/share/python/graphite/bin/pip install blist
 
 find ${BUILD_DIR} ! -perm -a+r -exec chmod a+r {} \;
 
-mkdir -p ${BUILD_DIR}/usr/share/python/graphite/lib
-cd ${BUILD_DIR}/usr/share/python/graphite/lib
-ln -s ../lib-python/2.7 python2.7
-cd ..
-${BUILD_DIR}/usr/share/python/graphite/bin/virtualenv-tools --update-path /usr/share/python/graphite
-#$BUILD_DIR/usr/share/python/graphite/bin/python -m compileall /usr/share/python/graphite/lib > /dev/null
-#$BUILD_DIR/usr/share/python/graphite/bin/python -O -m compileall /usr/share/python/graphite/lib > /dev/null
-
-#find ${BUILD_DIR} -iname *.pyc -exec rm {} \;
-#find ${BUILD_DIR} -iname *.pyo -exec rm {} \;
+cd ${BUILD_DIR}/usr/share/python/graphite
+virtualenv-tools --update-path /usr/share/python/graphite
 
 #mkdir -p ${BUILD_DIR}/etc
 cp -a ${DIR}/config/common ${BUILD_DIR}/common
