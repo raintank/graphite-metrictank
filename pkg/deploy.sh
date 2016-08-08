1cb9776542d79c4d0ffb9b0600b9bb746c8e23b9
woodsaj <anthony@monkey.id.au>
2016-08-08 15:28:42 +0800

    fix path to packages to upload to packagecloud

--- pkg/deploy.sh
+++ pkg/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 : ${BUILD_ROOT:="${DIR}/build"}
@@ -9,9 +9,9 @@ if [ -z ${PACKAGECLOUD_REPO} ] ; then
   exit 1
 fi
 
-package_cloud push ${PACKAGECLOUD_REPO}/debian/wheezy ${BUILD_ROOT}/sysvinit/*deb
-package_cloud push ${PACKAGECLOUD_REPO}/debian/jessie ${BUILD_ROOT}/systemd/*deb
-package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/trusty ${BUILD_ROOT}/upstart/*deb
-package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/xenial ${BUILD_ROOT}/systemd/*deb
-package_cloud push ${PACKAGECLOUD_REPO}/el/6 ${BUILD_ROOT}/upstart-0.6.5/*rpm
-package_cloud push ${PACKAGECLOUD_REPO}/el/7 ${BUILD_ROOT}/systemd-centos7/*rpm
+package_cloud push ${PACKAGECLOUD_REPO}/debian/wheezy ${BUILD_ROOT}/pkg/debian/wheezy/*.deb
+package_cloud push ${PACKAGECLOUD_REPO}/debian/jessie ${BUILD_ROOT}/pkg/debian/jessie/*.deb
+package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/trusty ${BUILD_ROOT}/pkg/ubuntu/trusty/*.deb
+package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/xenial ${BUILD_ROOT}/pkg/ubuntu/xenial/*.deb
+package_cloud push ${PACKAGECLOUD_REPO}/el/6 ${BUILD_ROOT}/pkg/centos/6/*.rpm
+package_cloud push ${PACKAGECLOUD_REPO}/el/7 ${BUILD_ROOT}/pkg/centos/7/*.rpm
