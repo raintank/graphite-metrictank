533f005fd24898ae2cd5b1b987870977dfda55e9
woodsaj <awoods@raintank.io>
2016-12-19 12:23:01 +0800

    make package building more flexible
    - make it easier to build for specific distros

--- pkg/deploy.sh
+++ pkg/deploy.sh
@@ -9,9 +9,26 @@ if [ -z ${PACKAGECLOUD_REPO} ] ; then
   exit 1
 fi
 
-package_cloud push ${PACKAGECLOUD_REPO}/debian/wheezy ${BUILD_ROOT}/pkg/debian/wheezy/*.deb
-package_cloud push ${PACKAGECLOUD_REPO}/debian/jessie ${BUILD_ROOT}/pkg/debian/jessie/*.deb
-package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/trusty ${BUILD_ROOT}/pkg/ubuntu/trusty/*.deb
-package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/xenial ${BUILD_ROOT}/pkg/ubuntu/xenial/*.deb
-#package_cloud push ${PACKAGECLOUD_REPO}/el/6 ${BUILD_ROOT}/pkg/centos/6/*.rpm
-#package_cloud push ${PACKAGECLOUD_REPO}/el/7 ${BUILD_ROOT}/pkg/centos/7/*.rpm
+if [ -d ${BUILD_ROOT}/pkg/debian/wheezy ]; then
+	package_cloud push ${PACKAGECLOUD_REPO}/debian/wheezy ${BUILD_ROOT}/pkg/debian/wheezy/*.deb
+fi
+
+if [ -d ${BUILD_ROOT}/pkg/debian/jessie ]; then
+	package_cloud push ${PACKAGECLOUD_REPO}/debian/jessie ${BUILD_ROOT}/pkg/debian/jessie/*.deb
+fi
+
+if [ -d ${BUILD_ROOT}/pkg/ubuntu/trusty ]; then
+	package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/trusty ${BUILD_ROOT}/pkg/ubuntu/trusty/*.deb
+fi
+
+if [ -d ${BUILD_ROOT}/pkg/ubuntu/xenial ]; then
+	package_cloud push ${PACKAGECLOUD_REPO}/ubuntu/xenial ${BUILD_ROOT}/pkg/ubuntu/xenial/*.deb
+fi
+
+if [ -d ${BUILD_ROOT}/pkg/centos/6 ]; then
+	package_cloud push ${PACKAGECLOUD_REPO}/el/6 ${BUILD_ROOT}/pkg/centos/6/*.rpm
+fi
+
+if [ -d ${BUILD_ROOT}/pkg/centos/7 ]; then
+	package_cloud push ${PACKAGECLOUD_REPO}/el/7 ${BUILD_ROOT}/pkg/centos/7/*.rpm
+fi
--- pkg/package.sh
+++ pkg/package.sh
@@ -11,127 +11,134 @@ ARCH="$(uname -m)"
 # wheezy, other sysvinit
 BUILD_ROOT=${DIR}/build
 BUILD="${BUILD_ROOT}/debian/wheezy"
-PKG_DIR="${DIR}/build/pkg/debian/wheezy"
-mkdir -p $PKG_DIR
-PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
+if [ -d $BUILD ]; then
+  PKG_DIR="${DIR}/build/pkg/debian/wheezy"
+  mkdir -p $PKG_DIR
+  PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
 
-fpm \
-  -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
-  --config-files /etc/graphite-metrictank/ \
-  --config-files /etc/default/graphite-metrictank \
-  -d libcairo2 \
-  -d python \
-  -d "libffi5 | libffi6" \
-  --after-install ${DIR}/debian/scripts/post-install \
-  --before-remove ${DIR}/debian/scripts/pre-remove \
-  --after-remove ${DIR}/debian/scripts/post-remove \
-  --url https://github.com/raintank/graphite-metrictank \
-  --description 'finder plugin to use metrictank with graphite-api' \
-  --license 'Apache 2.0' \
-  --replaces graphite-raintank \
-  -p ${PACKAGE_NAME} .
+  fpm \
+    -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
+    --config-files /etc/graphite-metrictank/ \
+    --config-files /etc/default/graphite-metrictank \
+    -d libcairo2 \
+    -d python \
+    -d "libffi5 | libffi6" \
+    --after-install ${DIR}/debian/scripts/post-install \
+    --before-remove ${DIR}/debian/scripts/pre-remove \
+    --after-remove ${DIR}/debian/scripts/post-remove \
+    --url https://github.com/raintank/graphite-metrictank \
+    --description 'finder plugin to use metrictank with graphite-api' \
+    --license 'Apache 2.0' \
+    --replaces graphite-raintank \
+    -p ${PACKAGE_NAME} .
+fi
 
 # ubuntu 14.04
 
 BUILD="${BUILD_ROOT}/ubuntu/trusty"
-PKG_DIR="${DIR}/build/pkg/ubuntu/trusty"
-mkdir -p $PKG_DIR
-PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
+if [ -d $BUILD ]; then
+  PKG_DIR="${DIR}/build/pkg/ubuntu/trusty"
+  mkdir -p $PKG_DIR
+  PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
 
-
-fpm \
-  -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
-  --config-files /etc/graphite-metrictank/ \
-  --config-files /etc/default/graphite-metrictank \
-  --after-install ${DIR}/ubuntu/scripts/post-install.sh \
-  -d libcairo2 \
-  -d python \
-  -d "libffi5 | libffi6" \
-  --url https://github.com/raintank/graphite-metrictank \
-  --description 'finder plugin to use metrictank with graphite-api' \
-  --license 'Apache 2.0' \
-  --replaces graphite-raintank \
-  -p ${PACKAGE_NAME} .
+  fpm \
+    -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
+    --config-files /etc/graphite-metrictank/ \
+    --config-files /etc/default/graphite-metrictank \
+    --after-install ${DIR}/ubuntu/scripts/post-install.sh \
+    -d libcairo2 \
+    -d python \
+    -d "libffi5 | libffi6" \
+    --url https://github.com/raintank/graphite-metrictank \
+    --description 'finder plugin to use metrictank with graphite-api' \
+    --license 'Apache 2.0' \
+    --replaces graphite-raintank \
+    -p ${PACKAGE_NAME} .
+fi
 
 # ubuntu 16.04
-
 BUILD="${BUILD_ROOT}/ubuntu/xenial"
-PKG_DIR="${DIR}/build/pkg/ubuntu/xenial"
-mkdir -p $PKG_DIR
-PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
+if [ -d $BUILD ]; then
+  PKG_DIR="${DIR}/build/pkg/ubuntu/xenial"
+  mkdir -p $PKG_DIR
+  PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
 
-fpm \
-  -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
-  --config-files /etc/graphite-metrictank/ \
-  --config-files /etc/default/graphite-metrictank \
-  --after-install ${DIR}/ubuntu/scripts/post-install.sh \
-  -d libcairo2 \
-  -d python \
-  -d "libffi5 | libffi6" \
-  --url https://github.com/raintank/graphite-metrictank \
-  --description 'finder plugin to use metrictank with graphite-api' \
-  --license 'Apache 2.0' \
-  --replaces graphite-raintank \
-  -p ${PACKAGE_NAME} .
+  fpm \
+    -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
+    --config-files /etc/graphite-metrictank/ \
+    --config-files /etc/default/graphite-metrictank \
+    --after-install ${DIR}/ubuntu/scripts/post-install.sh \
+    -d libcairo2 \
+    -d python \
+    -d "libffi5 | libffi6" \
+    --url https://github.com/raintank/graphite-metrictank \
+    --description 'finder plugin to use metrictank with graphite-api' \
+    --license 'Apache 2.0' \
+    --replaces graphite-raintank \
+    -p ${PACKAGE_NAME} .
+fi
+
 
 # debian jessie
-
 BUILD="${BUILD_ROOT}/debian/jessie"
-PKG_DIR="${DIR}/build/pkg/debian/jessie"
-mkdir -p $PKG_DIR
-PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
+if [ -d $BUILD ]; then
+  PKG_DIR="${DIR}/build/pkg/debian/jessie"
+  mkdir -p $PKG_DIR
+  PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}_${ARCH}.deb"
 
-fpm \
-  -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
-  --config-files /etc/graphite-metrictank/ \
-  --config-files /etc/default/graphite-metrictank \
-  --after-install ${DIR}/ubuntu/scripts/post-install.sh \
-  -d libcairo2 \
-  -d python \
-  -d "libffi5 | libffi6" \
-  --url https://github.com/raintank/graphite-metrictank \
-  --description 'finder plugin to use metrictank with graphite-api' \
-  --license 'Apache 2.0' \
-  --replaces graphite-raintank \
-  -p ${PACKAGE_NAME} .
+  fpm \
+    -t deb -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
+    --config-files /etc/graphite-metrictank/ \
+    --config-files /etc/default/graphite-metrictank \
+    --after-install ${DIR}/ubuntu/scripts/post-install.sh \
+    -d libcairo2 \
+    -d python \
+    -d "libffi5 | libffi6" \
+    --url https://github.com/raintank/graphite-metrictank \
+    --description 'finder plugin to use metrictank with graphite-api' \
+    --license 'Apache 2.0' \
+    --replaces graphite-raintank \
+    -p ${PACKAGE_NAME} .
+fi
 
-
-exit
 # centos 6
-
 BUILD="${BUILD_ROOT}/centos/6"
-PKG_DIR="${DIR}/build/pkg/centos/6"
-mkdir -p $PKG_DIR
-PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}.el6.${ARCH}.rpm"
+if [ -d $BUILD ]; then
+  PKG_DIR="${DIR}/build/pkg/centos/6"
+  mkdir -p $PKG_DIR
+  PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}.el6.${ARCH}.rpm"
 
-fpm \
-  -t rpm -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
-  --config-files /etc/graphite-metrictank/ \
-  --config-files /etc/default/graphite-metrictank \
-  --after-install ${DIR}/centos/scripts/post-install.sh \
-  -d cairo \
-  -d libffi \
-  --url https://github.com/raintank/graphite-metrictank \
-  --description 'finder plugin to use metrictank with graphite-api' \
-  --license 'Apache 2.0' \
-  --replaces graphite-raintank \
-  -p ${PACKAGE_NAME} .
+  fpm \
+    -t rpm -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
+    --config-files /etc/graphite-metrictank/ \
+    --config-files /etc/default/graphite-metrictank \
+    --after-install ${DIR}/centos/scripts/post-install.sh \
+    -d cairo \
+    -d libffi \
+    --url https://github.com/raintank/graphite-metrictank \
+    --description 'finder plugin to use metrictank with graphite-api' \
+    --license 'Apache 2.0' \
+    --replaces graphite-raintank \
+    -p ${PACKAGE_NAME} .
+fi
 
 # centos 7
 BUILD="${BUILD_ROOT}/centos/7"
-PKG_DIR="${DIR}/build/pkg/centos/7"
-mkdir -p $PKG_DIR
-PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}.el7.${ARCH}.rpm"
+if [ -d $BUILD ]; then
+  PKG_DIR="${DIR}/build/pkg/centos/7"
+  mkdir -p $PKG_DIR
+  PACKAGE_NAME="${PKG_DIR}/${NAME}-${VERSION}.el7.${ARCH}.rpm"
 
-fpm \
-  -t rpm -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
-  --config-files /etc/graphite-metrictank/ \
-  --config-files /etc/default/graphite-metrictank \
-  --after-install ${DIR}/centos/scripts/post-install.sh \
-  -d cairo \
-  -d libffi \
-  --url https://github.com/raintank/graphite-metrictank \
-  --description 'finder plugin to use metrictank with graphite-api' \
-  --license 'Apache 2.0' \
-  --replaces graphite-raintank \
-  -p ${PACKAGE_NAME} .
+  fpm \
+    -t rpm -s dir -C ${BUILD} -n ${NAME} -v $VERSION \
+    --config-files /etc/graphite-metrictank/ \
+    --config-files /etc/default/graphite-metrictank \
+    --after-install ${DIR}/centos/scripts/post-install.sh \
+    -d cairo \
+    -d libffi \
+    --url https://github.com/raintank/graphite-metrictank \
+    --description 'finder plugin to use metrictank with graphite-api' \
+    --license 'Apache 2.0' \
+    --replaces graphite-raintank \
+    -p ${PACKAGE_NAME} .
+fi
